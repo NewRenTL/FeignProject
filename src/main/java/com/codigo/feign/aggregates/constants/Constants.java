3d0f06f550127bd350aa53ec213497ef055817e0
NewRenTL <diego.bustamante@utec.edu.pe>
2024-02-09 19:52:44 -0500

    I'll push my repositories,response and constants

---
+++ src/main/java/com/codigo/feign/aggregates/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.codigo.feign.aggregates.constants;
+
+public class Constants {
+    public static final Integer CODE_SUCCESS = 200;
+
+    public static final Integer CODE_ERROR = 400;
+
+    public static final String MESS_SUCCESS = "Correct Execution";
+
+    public static final String MESS_ERROR = "Execution error";
+}
--- src/main/java/com/codigo/feign/aggregates/requests/PersonaRequest.java
+++ src/main/java/com/codigo/feign/aggregates/requests/PersonaRequest.java
@@ -2,6 +2,8 @@ package com.codigo.feign.aggregates.requests;
 
 import jakarta.persistence.Id;
 
+
+//This is what we send within the body
 public class PersonaRequest {
     private String tipoDoc;
     private String numDoc;
